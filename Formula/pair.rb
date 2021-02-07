84973de37a3e769c2569171c84fd26111cdb8c66
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-07 12:51:48 +0000

    Brew formula update for pair version v0.1.3

--- Formula/pair.rb
+++ Formula/pair.rb
@@ -5,17 +5,17 @@
 class Pair < Formula
   desc "A tool for terminal based pair programming"
   homepage "https://github.com/bottlerocketlabs/pair"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bottlerocketlabs/pair/releases/download/v0.1.2/pair_0.1.2_Darwin_amd64.tar.gz"
-    sha256 "77cde27f3a87c701803e0f34e84100086a3d0ce95f9874a36c99dcaebc6e7984"
+    url "https://github.com/bottlerocketlabs/pair/releases/download/v0.1.3/pair_0.1.3_Darwin_amd64.tar.gz"
+    sha256 "24312963fbe964b77504d57883732f5aa78737ef473ddd1c8bf17a2cd2b834d5"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bottlerocketlabs/pair/releases/download/v0.1.2/pair_0.1.2_Linux_amd64.tar.gz"
-    sha256 "e246b5b445c8ef3333fb7c70f1cecaefa0955e833f6447097e8bb3831d70be2a"
+    url "https://github.com/bottlerocketlabs/pair/releases/download/v0.1.3/pair_0.1.3_Linux_amd64.tar.gz"
+    sha256 "7f7d4b3d1b80302bc0a335ad906223325cd56522142b047b2b2fbc80aa3fcf05"
   end
 
   depends_on "go"
